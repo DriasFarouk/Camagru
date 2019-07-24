15be2781cf70a0226fb7c629cbcde288d13f48bd
Farouk DRIAS <fdrias@e1r4p9.42.fr>
2019-07-24 14:21:32 +0200

    AddNewContent

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div class="footer">
+	<div class="section">
+		<ul class="section">
+			<li class="section">
+				<a class="section_footer">Camagru</a>
+			</li>
+		</ul>
+	</div>
+	<div class="right">
+		<ul class="section">
+			<li class="section">
+				<a class="section_footer">fdrias</a>
+			</li>
+		</ul>
+	</div>
+</div>
